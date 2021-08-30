5f3ad119add6a3a2d13ea70599756e45c2cb0215
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:01:38 +0530

    Add project files.

---
+++ ConsoleAppSwap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Demo6
+{
+    class Swap
+    {
+        public void SwapNum(ref int x, ref int y)
+        {
+            int z = x;
+            x = y;
+            y = z;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 30;
+            Swap s1 = new Swap();
+            Console.WriteLine("Value of a and b before Swapping");
+            Console.WriteLine("a =" + " " + a);
+            Console.WriteLine("b =" + " " + b);
+            s1.SwapNum(ref a, ref b);
+            Console.WriteLine("Value of a and b After Swapping");
+            Console.WriteLine("a =" + " " + a);
+            Console.WriteLine("b =" + " " + b);
+            Console.ReadKey();
+
+        }
+    }
+}
